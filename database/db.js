2d4a5dd4a53de7b4256424719e3350af51137a21
Tomas Benitez <tomasbenitezc13@gmail.com>
2022-06-25 21:30:11 -0300

    make4

--- database/db.js
+++ database/db.js
@@ -1,7 +1,8 @@
 import { Sequelize } from "sequelize";
 
 const db = new Sequelize('database_app', 'root', '',{
-    host: 'mysql',
+    host: '127.0.0.1',
+    port: '3306',
     dialect: 'mysql'
 })
 
